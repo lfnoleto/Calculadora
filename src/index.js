2a238f6dcc02b537c062d3ca4d738d2215da59f1
LUCAS FERREIRA NOLETO <LFNOLETO@jmnetid.com>
2021-02-08 11:48:17 -0300

    feat main
    Feat  styles mai

--- src/index.js
+++ src/index.js
@@ -1,63 +1,139 @@
 import React,{useState} from 'react'
-import {StatusBar,StyleSheet,Text,View,TouchableOpacity} from 'react-native'
+import {StatusBar,StyleSheet,Text,View,TouchableOpacity, Button} from 'react-native'
+import { set } from 'react-native-reanimated';
 import Icon from 'react-native-vector-icons/Feather';
 
 
-export default function App(){
-
-    const [dorkMode,setDackMode] = useState(false)
-
+export default function App() {
+    const [darkMode, setDackMode] = useState(false)
+    const buttons = ['AC', 'DEL', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 3, 2, 1, '+', 0, '.', '+/-', '=']  
+  
+    const [currentNumber, setCurrentNumber] = useState("")
+    const [lastNumber, setLastNumber] = useState("")
+  
+  
+    function calculator(){
+      const splitNumbers = currentNumber.split(' ')
+      const fistNumber = parseFloat(splitNumbers[0])
+      const lastNumber = parseFloat(splitNumbers[2])
+      const operator = splitNumbers[1]
+  
+      switch(operator){
+        case '+':
+          setCurrentNumber((fistNumber + lastNumber).toString())
+          return
+        case '-': 
+          setCurrentNumber((fistNumber - lastNumber).toString())
+          return
+        case '*':
+          setCurrentNumber((fistNumber * lastNumber).toString())
+          return
+        case '/': 
+          setCurrentNumber((fistNumber / lastNumber).toString())
+          return
+      }
+    }
+  
+    function handleInput(buttonPressed){
+      console.log(buttonPressed)
+      if(buttonPressed === '+' | buttonPressed === "-" | buttonPressed === "*" | buttonPressed === "/" ){
+        setCurrentNumber(currentNumber + " " + buttonPressed + " ")
+        return
+      }
+      switch(buttonPressed){
+        case 'DEL':
+          setCurrentNumber(currentNumber.substring(0, (currentNumber.length -1)))
+          return
+        case 'AC':
+          setLastNumber("")
+          setCurrentNumber("")
+          return
+        case '=':
+          setLastNumber(currentNumber + " = ")
+          calculator()
+          return
+        case '+/-':
+          return
+      }
+  
+      setCurrentNumber(currentNumber + buttonPressed)
+    }
+  
     const styles = StyleSheet.create({
-        conteiner:{
-            flex:1,
-            backgroundColor:'#fff',
-            alignItems:'center',
-            justifyContent:'center'
-        },
-        results:{
-            backgroundColor: dorkMode ? '#282f3b' : '#f5f5f5',
-            minWidth:280,
-            width:'100%',
-            height:'59%',
-            alignItems:'flex-end',
-            justifyContent:'flex-end',
-           
-           
-        },
-        themaButton:{
-            alignSelf:'flex-start',
-            bottom:130,
-            margin:15,
-            backgroundColor: dorkMode ? '#7b8084' : "#e5e5e5",
-            alignItems: 'center',
-            justifyContent:'center',
-            width:50,
-            height:50,
-            borderRadius:25
-        },
-        buttons:{
-            
-        },
-        resultText:{
-            margin:10,
-            fontSize:25
-        }
-    })
+      results: {
+        backgroundColor: darkMode ? "#282f3b" : "#f5f5f5",
+        width: '100%',
+        minHeight: 280,
+        alignItems: 'flex-end',
+        justifyContent: 'flex-end'
+      },
+      resultText: {
+        color: darkMode ? "#f5f5f5" : "#282F38",
+        margin: 10,
+        fontSize: 40
+      },
+  
+      historyText:{
+        color: darkMode ? "#B5B7BB" : "#7c7c7c",
+        fontSize: 20,
+        marginRight: 10,
+        alignSelf: 'flex-end',
+      },
+      themeButton: {
+        alignSelf: 'flex-start',
+        bottom: 120,
+        margin: 10,
+        backgroundColor: darkMode ? "#7b8084" :"#e5e5e5",
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        
+      },
+      buttons: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+      },
+      button: {
+        borderColor: darkMode ? '#3f4d5b' : "#e5e5e5",
+        borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        minWidth: 90, 
+        minHeight: 90,
+        flex: 2,
+      },
+      textButton: {
+        color: darkMode ? "#b5b7bb" : "#7c7c7c",
+        fontSize: 20,
+      }, 
+    });
 
-
-    return(
-        <View >
-
-            <View style={styles.results}>
-                <TouchableOpacity style={styles.themaButton}>
-                <Icon name={dorkMode ? "sun" : "moon"} size={18} color={dorkMode ? "white" : "black"}  onPress={() => dorkMode ? setDackMode(false) : setDackMode(true)}/>
-                </TouchableOpacity>
-                <Text style={styles.resultText}>2 + 2 = 10</Text>
-            </View>
-
-            <View style={styles.buttons}>
-
-            </View>
+    return (
+        <View>
+            <StatusBar backgroundColor= {darkMode ? "#282f3b" : "#f5f5f5"} />
+          <View style={styles.results}>
+            <TouchableOpacity style={styles.themeButton}>
+            <Icon name={darkMode ? "sun" : "moon"} size={18} color={darkMode ? "white" : "black"}  onPress={() => darkMode ? setDackMode(false) : setDackMode(true)}/>
+            </TouchableOpacity>
+            <Text style={styles.historyText}>{lastNumber}</Text>
+            <Text style={styles.resultText}>{currentNumber}</Text>
+          </View>
+          <View style={styles.buttons}>
+            {buttons.map((button) => 
+              button === '=' ?
+            <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#9DBC7B'}]}>
+              <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
+            </TouchableOpacity>
+              :
+              <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, 
+              {backgroundColor: typeof(button) === 'number' ? darkMode === true ? '#303946' : '#fff': darkMode === true ? '#414853' : '#ededed'}]}>
+                <Text style={styles.textButton}>{button}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
         </View>
-    )
-}
+      );
+    }
+    
